2b669cce7954e495290550c7ab443cec0f3013ac
oaxiom <oaxiom@localhost>
2015-01-16 11:10:12 +0800

    RNABPDB mm9 hg19

---
+++ rna_binding_proteins/mm9/pack_data.py
@@ -0,0 +1,10 @@
+
+
+from glbase import *
+
+data = genelist(filename="RBPDB_v1.3.1_proteins_mouse_2012-11-21.csv", format={"ensg": 1, "name": 4, "domain": 8, "skiplines": -1})
+
+print data
+
+data.saveTSV("RBP_mm9.tsv", key_order=["ensg", "name", "domain"])
+data.save("RBP_mm9.glb")
