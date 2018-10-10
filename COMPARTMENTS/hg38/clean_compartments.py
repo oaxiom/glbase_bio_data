98a69e437257c3c457fce76c012579f9fa599285
oaxiom <oaxiom@localhost>
2018-10-10 10:44:40 +0800

    added compartments

---
+++ COMPARTMENTS/hg38/clean_compartments.py
@@ -0,0 +1,57 @@
+
+"""
+
+clean compartments
+
+"""
+
+import operator
+from glbase3 import *
+
+comparts_to_get = ["Nucleus", "Cytoskeleton", "Plasma membrane", "Golgi apparatus", "Endosome",
+    'Cytoplasm', 'Lysosome', 'Mitochondrion', ]
+
+comparts = {}
+cindex = 0
+genes = {}
+oh = open("human_compartment_knowledge_full.tsv", "rU")
+for lin in oh:
+    lin = lin.strip().split("\t")
+               
+    if lin[1] not in genes:
+        genes[lin[1]] = {"name": lin[1], "ensp": lin[0], "conditions": []}
+    
+    # There are hundreds of compartments, I only what a selection:
+    if lin[3] not in comparts_to_get: # Here so every gene will get filled in
+        continue    
+
+    if lin[3] not in comparts:
+        comparts[lin[3]] = cindex
+        cindex += 1
+    
+    while len(genes[lin[1]]["conditions"]) < cindex:
+        genes[lin[1]]["conditions"].append(0)
+    
+    if int(lin[6]) >= 3: # Only use >3 confidence
+        genes[lin[1]]["conditions"][comparts[lin[3]]] = int(lin[6])
+ 
+oh.close()
+
+# Fill in any missing entries in "conditions"
+for gene in genes:
+    while len(genes[gene]["conditions"]) < cindex:
+        genes[gene]["conditions"].append(0)
+        
+conds = zip(comparts.keys(), comparts.values())
+conds = sorted(conds, key=operator.itemgetter(1))
+print(conds)
+conds = [i[0] for i in conds]
+
+expn = expression(loadable_list=[genes[k] for k in genes], cond_names=conds)
+expn.saveTSV("hg38_compartments_selected.tsv", key_order=["name"])
+expn.save("hg38_compartments_selected.glb")
+
+print(expn)
+
+expn.heatmap(filename="compartments.png", bracket=[0, 5], col_cluster=False)
+
