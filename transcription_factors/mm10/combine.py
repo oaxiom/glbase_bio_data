9619f6d29cac30edb3dcc2bea108f8306f6d9daa
oaxiom <oaxiom@gmail.com>
2020-04-28 08:37:56 +0800

    updated transcription factor category to glbase3

--- transcription_factors/mm10/combine.py
+++ transcription_factors/mm10/combine.py
@@ -4,26 +4,28 @@
 Merge TF DBD and GO category into a single file containing ENSG and name
 
 """
-
+import sys, os
 from glbase3 import *
 
 map_form = {"force_tsv": True, "ensg": 0, "enst": 1, "ensp": 2, "name": 3}
 
-mapper = genelist(os.path.join(os.path.expanduser("~"), "mm10/mm9.ensg.enst.ensp.name.v67.tsv"), format=map_form)
+mapper = genelist(os.path.join(os.path.expanduser("~"), "mm10/mm10_ensg.enst.ensp.name.v99.tsv"), format=map_form)
 
 tfdbd_form = {"force_tsv": True, "ensp": 1}
-anitf_form = {"force_tsv": True, "ensg": 1, "name": 3}
+anitf_form = {"force_tsv": True, "ensg": 0, "name": 3}
 
 tfdbd = genelist("TFDBD_table.tsv", format=tfdbd_form)
 tfdbd.save("all_tfdbd.glb")
-tfcla = glload("TFClass.mm9.glb")
-anitf = genelist("AnimalTFDB.tsv", format=anitf_form)
 
+#tfcla = glload("TFClass.mm9.glb")
+anitf = genelist("AnimalTFDB.tsv", format=anitf_form)
+print(anitf)
 # map all relative to ensg:
 tfdbd = mapper.map(tfdbd, key="ensp").removeDuplicates("ensg")
 
-all_tfs = tfdbd + tfcla + anitf
+all_tfs = tfdbd + anitf
 all_tfs = all_tfs.removeDuplicates("ensg")
+print(all_tfs)
 
 all_tfs = all_tfs.getColumns(["ensg", "name"])
 all_tfs.sort("name")
@@ -32,5 +34,5 @@ all_tfs.save("all_tfs.glb")
 
 print("Finally", len(all_tfs), "tfs")
 
-gl = glglob(tfdbd, tfcla, anitf)
+gl = glglob(tfdbd, anitf)
 gl.venn(filename="overlap.png", key="ensg")
--- transcription_factors/mm10/proc_animalTFDB.py
+++ transcription_factors/mm10/proc_animalTFDB.py
@@ -4,24 +4,26 @@ process the Animal TFDB data which is a bloody mess.
 
 """
 
-from glbase import *
+import sys, os
+from glbase3 import *
 
 map_form = {"force_tsv": True, "ensg": 0, "enst": 1, "ensp": 2, "name": 3}
-mapper = genelist(os.path.join(os.path.expanduser("~"), "mm9/mm9.ensg.enst.ensp.name.v67.tsv"), format=map_form)
+mapper = genelist(os.path.join(os.path.expanduser("~"), "mm10/mm10_ensg.enst.ensp.name.v99.tsv"), format=map_form)
 
-oh = open("AnimalTFDB.raw.txt", "rU")
+oh = open("AnimalTFDB.raw.txt", "rt")
 
 res = []
 for line in oh:
     tt = line.split()
-    res.append({"ensg": tt[1]})
-    
+    print(tt)
+    res.append({"ensg": tt[0]})
+
 oh.close()
 
 gl = genelist()
 gl.load_list(res)
 
-print gl
+print(gl)
 
 gl = gl.map(genelist=mapper, key="ensg").removeDuplicates("ensg")
-gl.saveTSV("AnimalTFDB.tsv")
+gl.saveTSV("AnimalTFDB.tsv", key_order=['ensg'])
