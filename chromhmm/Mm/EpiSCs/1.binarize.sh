5cc0c6c67bb93d0106435383f8f85d35ad86cdd2
oaxiom <oaxiom@gmail.com>
2023-04-10 17:09:36 +0800

    some ChromHMM ideas

---
+++ chromhmm/Mm/EpiSCs/1.binarize.sh
@@ -0,0 +1,4 @@
+
+# These are SE reads
+java -mx4000M -jar ~/Tools/ChromHMM.jar BinarizeBam -mixed ../mm10.genome cellmarkfiletable.tsv episc_bams
+
---
+++ chromhmm/Mm/EpiSCs/2.learnmodel.sh
@@ -0,0 +1,3 @@
+
+java -mx4000M -jar ChromHMM.jar LearnModel mESC_E14_BinarizeBamDir/ mESC_E14_ChromHMM_output/ 12 mm10
+
