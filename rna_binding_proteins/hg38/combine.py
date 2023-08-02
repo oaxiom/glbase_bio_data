db2c5ad9cb62a639ab57302909dd426851d0cf99
oaxiom <oaxiom@gmail.com>
2023-08-02 11:37:56 +0800

    added EuRBPDB to hg38

--- rna_binding_proteins/hg38/combine.py
+++ rna_binding_proteins/hg38/combine.py
@@ -1,17 +1,22 @@
 """
 
-Merge TF DBD and GO category into a single file containing ENSG and name
+Merge RBP databases
 
 """
 
 import os, sys
 from glbase3 import *
 
-rbp_form = {"force_tsv": False, "ensg": 1, 'name': 4}
+eurbpdb = genelist(filename='EuRBPDB/Homo_sapiens.RBP.txt.gz', gzip=True,
+    format={'skiplines': -1, 'force_tsv': True, 'ensg': 0, 'name': 1, 'domain': 4})
 
-dbd = genelist("RBPDB_v1.3.1_proteins_human_2012-11-21.csv.gz", format=rbp_form, gzip=True)
-dbd.save('rbp_db_hg38.glb')
-dbd.saveTSV('rbp_db_hg38.tsv', key_order=['ensg', 'enst', 'ensp', 'name'])
+dbd = genelist("RBPDB_v1.3.1_proteins_human_2012-11-21.csv.gz",
+    format={"force_tsv": False, "ensg": 1, 'name': 4, 'domain': 8},
+    gzip=True)
 
-print()
-print(dbd)
+data = dbd + eurbpdb
+data = data.removeDuplicates('ensg')
+
+data.save('rbp_db_hg38.glb')
+data.saveTSV('rbp_db_hg38.tsv', key_order=['ensg', 'name', 'domain'])
+
