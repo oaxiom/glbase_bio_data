da35771c15cf0f259fe42761b9f6c7c6467b560c
oaxiom <oaxiom@gmail.com>
2023-08-02 11:34:09 +0800

    added EuRBPDB data to mm10

--- rna_binding_proteins/mm10/pack_data.py
+++ rna_binding_proteins/mm10/pack_data.py
@@ -4,7 +4,12 @@ from glbase3 import *
 
 data = genelist(filename="RBPDB_v1.3.1_proteins_mouse_2012-11-21.csv", format={"ensg": 1, "name": 4, "domain": 8, "skiplines": -1})
 
+eurbpdb = genelist(filename='EuRBPDB/Mus_musculus.RBP.txt.gz', gzip=True,
+    format={'skiplines': -1, 'force_tsv': True, 'ensg': 0, 'name': 1, 'domain': 5})
+
 print(data)
+
+data = data + eurbpdb
 
 data = data.removeDuplicates('ensg')
 
